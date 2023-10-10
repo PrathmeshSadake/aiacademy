07b1cdcad9b67ba4dc98bd7c0aa4de7e5fbcb3a3
Prathmesh Sadake <prathmeshsadake@gmail.com>
2023-10-10 13:20:25 +0530

    first commit

---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,72 @@
+import { useRef, useState } from "react";
+import toast from "react-hot-toast";
+import emailjs from "@emailjs/browser";
+
+const ContactForm = () => {
+  const form = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "shashwatupadhyay",
+        "shashwat-portfolio",
+        form.current,
+        "rc5rwbfKux-77Slax"
+      )
+      .then(
+        () => {
+          toast.success(
+            "Thank you for submitting your response. We will contact you within 2 business days."
+          );
+          setName("");
+          setEmail("");
+          setPhone("");
+        },
+        (error) => {
+          console.log(error);
+          toast.error("Something went wrong. Please try again later.");
+        }
+      );
+  };
+
+  return (
+    <form onSubmit={sendEmail} ref={form} className='form'>
+      <h3>Enquire Now</h3>
+      <input
+        required
+        type='text'
+        id='name'
+        name='name'
+        placeholder='Name'
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        required
+        type='text'
+        id='mobile'
+        name='phone'
+        placeholder='Mobile Number'
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <input
+        type='email'
+        id='email'
+        name='email'
+        placeholder='Email (Optional)'
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <button type='submit' id='submitButton'>
+        Enquire Now
+      </button>
+    </form>
+  );
+};
+
+export default ContactForm;
